cb67fb4d826727bbf77899ddc48a513a6557e253
larannma <larionova.a2000@gmail.com>
2024-05-27 18:23:04 +0000

    fea: create/get users

--- app.py
+++ app.py
@@ -1,29 +1,73 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 import pymongo
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
-# changes
+
+class User:
+    def __init__(self, db, chat_id):
+        self.db = db
+        self.collection = self.db.users
+        self.chat_id = chat_id
+        self.name = ""
+        self.email = ""
+
+    def create_user(self):
+        user = {
+            "chat_id": self.chat_id,
+            "name": self.name,
+            "email": self.email
+        }
+        result = self.collection.insert_one(user)
+        return str(result.inserted_id), user
 
 def get_db():
-    client = MongoClient(host='test_mongodb',
-                         port=27017, 
-                         username='root', 
-                         password='pass',
+    client = MongoClient(
+                        host='test_mongodb',
+                        port=27017, 
+                        username='root', 
+                        password='pass',
                         authSource="admin")
-    db = client["animal_db"]
+    db = client["ostrovok_db"]
     return db
 
-@app.route('/')
+@app.route('/ostrovok')
 def ping_server():
-    return "Welcome to the world of animals."
+    return "Welcome to ostrovok geoguesser battle!"
 
-@app.route('/animals')
-def get_stored_animals():
+# CREATE USER FOR THE FIRST TIME
+@app.route('/users', methods=['POST'])
+def create_user():
     db = get_db()
-    _animals = db.animal_tb.find()
-    animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
-    return jsonify({"animals": animals})
+    request_data = request.json
+    print("DATA",request_data)
+    user = User(db, chat_id=request_data.get("chat_id", ""))
+    user.create_user()
+    
+    return jsonify({
+        "_id": user.create_user()[0],
+        "chat_id": user.chat_id,
+        "name": user.name,
+        "email": user.email
+    }), 201
+
+# GET ALL USERS
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    db = get_db()
+    users = db.users.find()
+    user_list = []
+    for user in users:
+        user_dict = {
+            "_id": str(user["_id"]),
+            "chat_id": user.get("chat_id", ""),
+            "name": user.get("name", ""),
+            "email": user.get("email", "")
+        }
+        user_list.append(user_dict)
+    
+    return jsonify(user_list), 200
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=8000)
